4d60082911464e2bca102b86445437144603a111
Kryszak <13157331+Kryszak@users.noreply.github.com>
2022-05-13 10:17:46 +0200

    bot activity description update

--- index.js
+++ index.js
@@ -150,7 +150,7 @@ function verifyNewMessage(lastMessage) {
 
 client.on('ready', () => {
   logger.info(`Logged in as ${client.user.tag}!`);
-  client.user.setActivity('cyferki', { type: 'WATCHING'});
+  client.user.setActivity('grę w cyferki', { type: 'WATCHING'});
   client.guilds.cache.forEach((server) => printRolesGrantedForNumberOnServer(server));
 });
 
