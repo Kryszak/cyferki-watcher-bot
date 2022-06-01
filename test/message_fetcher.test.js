0a0f69bac782efcfcab5bf69a79f92c8c48cc446
Kryszak <prajskrzysztof@gmail.com>
2022-06-01 20:56:21 +0200

    cover message_fetcher with tests

---
+++ test/message_fetcher.test.js
@@ -0,0 +1,22 @@
+const globals = require('../src/globals');
+const {getLastMessagesFromWatchedChannel} = require('../src/discord/message_fetcher');
+
+jest.mock('../src/globals');
+
+beforeAll(() => {
+  globals.getReadMessagesCount.mockReturnValue(20);
+});
+
+test('Should fetch messages with given limit', () => {
+  const mockedFetch = jest.fn();
+  const channel = {
+    'messages': {
+      'fetch': mockedFetch,
+    },
+  };
+
+  getLastMessagesFromWatchedChannel(channel);
+
+  expect(mockedFetch.mock.calls.length).toBe(1);
+  expect(mockedFetch.mock.calls[0][0]).toStrictEqual({'limit': 20});
+});
