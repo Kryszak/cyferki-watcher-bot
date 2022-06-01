fe64270ab40a63b16b1e2d272a23a1c6e629360b
Kryszak <prajskrzysztof@gmail.com>
2022-06-01 20:51:39 +0200

    cover channel_utils with tests

--- test/channel_utils.test.js
+++ test/channel_utils.test.js
@@ -1,4 +1,4 @@
-const {isSentToWatchedChannel} = require('../src/discord/channel_utils');
+const {isSentToWatchedChannel, removeSendMessagePermissions, getChannel} = require('../src/discord/channel_utils');
 const globals = require('../src/globals');
 
 jest.mock('../src/globals');
@@ -13,6 +13,48 @@ test('Should return true for watched channel', () => {
   const channel = {
     'name': channelName,
   };
-
   expect(isSentToWatchedChannel(channel)).toBeTruthy();
+});
+
+test('Should return false for not watched channel', () => {
+  const channel = {
+    'name': 'general',
+  };
+  expect(isSentToWatchedChannel(channel)).toBeFalsy();
+});
+
+test('Should override permissions for channel', () => {
+  const mockedEdit = jest.fn(() => Promise.resolve());
+  const channel = {
+    'permissionOverwrites': {
+      'edit': mockedEdit,
+    },
+    'guild': {
+      'name': 'test guild',
+      'roles': {'everyone': true},
+    },
+  };
+  removeSendMessagePermissions(channel);
+
+  expect(mockedEdit.mock.calls.length).toBe(1);
+});
+
+test('Should return requested channel', () => {
+  const mockedGetChannel = jest.fn();
+  const client = {
+    'channels': {
+      'cache': {
+        'get': mockedGetChannel,
+      },
+    },
+  };
+
+  const message = {
+    'channelId': 123456,
+  };
+
+  getChannel(client, message);
+
+  expect(mockedGetChannel.mock.calls.length).toBe(1);
+  expect(mockedGetChannel.mock.calls[0][0]).toBe(123456);
 });
