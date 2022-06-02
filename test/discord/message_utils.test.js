7e97b69892b61512fc94d061c46052c864d58199
Kryszak <13157331+Kryszak@users.noreply.github.com>
2022-06-02 13:05:20 +0200

    add missing test cases for number extracting function

--- test/discord/message_utils.test.js
+++ test/discord/message_utils.test.js
@@ -1,4 +1,4 @@
-const {isSentFromUser, isContainingNumber, fetchMessage} = require('../../src/discord/message_utils');
+const {isSentFromUser, isContainingNumber, extractNumberFromMessage, fetchMessage} = require('../../src/discord/message_utils');
 
 test('Should return true for message sent from user', () => {
   const message = {
@@ -34,6 +34,22 @@ test('Should return false if message doesn\'t contain valid number', () => {
   };
 
   expect(isContainingNumber(message)).toBeFalsy();
+});
+
+test('Should return number if message contains valid number', () => {
+  const message = {
+    'content': '123 asdf testing correct message',
+  };
+
+  expect(extractNumberFromMessage(message)).toBe(123);
+});
+
+test('Should return NaN if message doesn\'t contain valid number', () => {
+  const message = {
+    'content': 'asdf testing incorrect message',
+  };
+
+  expect(isNaN(extractNumberFromMessage(message))).toBeTruthy();
 });
 
 test('Should fetch message', () => {
